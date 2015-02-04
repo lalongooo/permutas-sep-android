c201d64940dca11e36fcd77bf2345c77adf97565
lalongooo <hdez.jeduardo@gmail.com>
2015-02-04 11:21:33 -0600

    Added the "Terms of use" dialog

--- src/com/permutassep/ui/ActivityNewsFeed.java
+++ src/com/permutassep/ui/ActivityNewsFeed.java
@@ -1,9 +1,13 @@
 package com.permutassep.ui;
 
-import com.permutassep.R;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.permutassep.R;
 
 public class ActivityNewsFeed extends Activity {
 
@@ -12,5 +16,29 @@ public class ActivityNewsFeed extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_news_feed);
 	}
-	
+
+	public void testTOSDialog(View v) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(getString(R.string.tos_dialog_text))
+				.setPositiveButton(getString(R.string.tos_dialog_accept), dialogClickListener)
+				.setNegativeButton(getString(R.string.tos_dialog_cancel), dialogClickListener).show();
+	}
+
+	DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			switch (which) {
+			case DialogInterface.BUTTON_POSITIVE:
+				Toast.makeText(getApplicationContext(), "Yes clicked!", Toast.LENGTH_SHORT).show();
+				break;
+
+			case DialogInterface.BUTTON_NEGATIVE:
+				Toast.makeText(getApplicationContext(), "No clicked!",
+						Toast.LENGTH_SHORT).show();
+				break;
+			}
+		}
+	};
+
 }
