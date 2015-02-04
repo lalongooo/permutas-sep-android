9ab25e0dd911e87dfd2dae28fd14bf2834b8118c
lalongooo <hdez.jeduardo@gmail.com>
2015-02-04 10:45:02 -0600

    Added the News Feed activity (empty) and enhanced the "By lalongoo" message in the splash activity.

--- src/com/permutassep/ui/ActivityAppOverview.java
+++ src/com/permutassep/ui/ActivityAppOverview.java
@@ -1,21 +1,26 @@
 package com.permutassep.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.permutassep.R;
 import com.permutassep.adapter.AppOverviewAdapter;
 import com.viewpagerindicator.CirclePageIndicator;
 import com.viewpagerindicator.PageIndicator;
 
-public class ActivityAppOverview extends Activity {
+public class ActivityAppOverview extends Activity implements OnClickListener {
 
 	private PageIndicator indicator;
 	private ViewPager viewPager;
+	private Button btnStart;
 	int backgroundColors [] = {0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff};
 
 	@Override
@@ -24,7 +29,7 @@ public class ActivityAppOverview extends Activity {
 		this.setContentView(R.layout.activity_app_overview);
 		setUI();
 	}
-
+	
 	private void setUI() {
 		
         ColorDrawable[] colors = {new ColorDrawable(backgroundColors[3]), new ColorDrawable(backgroundColors[0])};
@@ -33,6 +38,8 @@ public class ActivityAppOverview extends Activity {
 		viewPager = (ViewPager) findViewById(R.id.pager);
 		viewPager.setBackgroundDrawable(trans);
 		indicator = (CirclePageIndicator) findViewById(R.id.indicator);
+		btnStart = (Button) findViewById(R.id.btnStart);
+		btnStart.setOnClickListener(this);
 
 		AppOverviewAdapter adapter = new AppOverviewAdapter(this);
 		viewPager.setAdapter(adapter);
@@ -55,6 +62,20 @@ public class ActivityAppOverview extends Activity {
 		    @Override
 		    public void onPageScrollStateChanged(int arg0) {}
 		});
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.btnStart:
+
+			Intent mainIntent = new Intent().setClass(ActivityAppOverview.this, ActivityNewsFeed.class);
+			startActivity(mainIntent);
+			finish();
+
+			break;
+		}
+
 	}
 
 }
---
+++ src/com/permutassep/ui/ActivityNewsFeed.java
@@ -0,0 +1,16 @@
+package com.permutassep.ui;
+
+import com.permutassep.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityNewsFeed extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_news_feed);
+	}
+	
+}
