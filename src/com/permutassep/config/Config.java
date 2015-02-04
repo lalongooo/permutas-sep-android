d3e232eaf0def4620fd70d8d44a73ca5ef2faec2
lalongooo <hdez.jeduardo@gmail.com>
2015-02-04 12:48:16 -0600

    Cleaned Config.java

--- src/com/permutassep/config/Config.java
+++ src/com/permutassep/config/Config.java
@@ -1,33 +1,12 @@
 package com.permutassep.config;
 
 public class Config {
-	
 
     /**
      * The URL of the web service for the users registration 
      */
-	public static final String USER_REGISTRARION_URL = "http://tlatoa.herokuapp.com/kerberos/api/user";
-	
-	/**
-	 * Tlatoa web service URL for translation
-	 */
-	public static final String PHRASE_TRANSLATION_URL = "http://tlatoa.herokuapp.com/manager/api/sentence?phrase=";
-	
-	/**
-	 * Enable caching.
-	 */
-	public static final boolean PHRASE_CACHE = false;
-	
-	/**
-	 * Default animation time of each image in the translation result activity (milliseconds). 
-	 */	
-	public static final int ANIMATION_DURATION = 500;
-	
-	/**
-	 * Default cache expiration time 
-	 */		
-	public static final long CACHE_EXPIRATION_VALID_TIME = 21600000;
-	
+	public static final String APP_PREFERENCES_NAME = "PERMUTAS_SEP_PREFERENCES";	
+
 	/**
 	 * The URL provided by Facebook for downloading the users profile pictures.
 	 */	
