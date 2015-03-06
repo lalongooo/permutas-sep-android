de9dac406bc57b4d6ca814dbb9c910efdc862e6e
lalongooo <hdez.jeduardo@gmail.com>
2015-03-05 19:05:51 -0600

    Tried to implemente the Material Drawer library

--- app/src/main/java/com/permutassep/ui/ActivityNewsFeed.java
+++ app/src/main/java/com/permutassep/ui/ActivityNewsFeed.java
@@ -1,20 +1,45 @@
 package com.permutassep.ui;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 
+import com.mikepenz.materialdrawer.Drawer;
 import com.permutassep.R;
 import com.permutassep.config.Config;
 
-public class ActivityNewsFeed extends Activity {
+public class ActivityNewsFeed extends ActionBarActivity {
+
+    public Drawer.Result result;
+
+    private OnFilterChangedListener onFilterChangedListener;
+
+    public void setOnFilterChangedListener(OnFilterChangedListener onFilterChangedListener) {
+        this.onFilterChangedListener = onFilterChangedListener;
+    }
+
+    public interface OnFilterChangedListener {
+        public void onFilterChanged(int filter);
+    }
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_news_feed);
-		
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
+
+        result = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withHeader(R.layout.header)
+                .build();
+        result.getListView().setVerticalScrollBarEnabled(false);
 	}
 	
 	private void showTOSDialog() {
