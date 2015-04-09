6283ba00d8f856dd76b2c4ece405aabd6e70cd79
lalongooo <hdez.jeduardo@gmail.com>
2015-04-08 20:54:32 -0500

    Removed redundant casting

--- app/src/main/java/com/permutassep/ui/FragmentCreatePost.java
+++ app/src/main/java/com/permutassep/ui/FragmentCreatePost.java
@@ -162,15 +162,15 @@ public class FragmentCreatePost extends Fragment implements
                                                         post.setUser(user);
                                                         break;
                                                     case PermutaSepWizardModel.CITY_FROM_KEY:
-                                                        State sf = (State) p.getData().getParcelable(ProfessorCityFromPage.STATE_DATA_KEY);
-                                                        City cf = (City) p.getData().getParcelable(ProfessorCityFromPage.MUNICIPALITY_DATA_KEY);
-                                                        Town tf = (Town) p.getData().getParcelable(ProfessorCityFromPage.LOCALITY_DATA_KEY);
+                                                        State sf = p.getData().getParcelable(ProfessorCityFromPage.STATE_DATA_KEY);
+                                                        City cf = p.getData().getParcelable(ProfessorCityFromPage.MUNICIPALITY_DATA_KEY);
+                                                        Town tf = p.getData().getParcelable(ProfessorCityFromPage.LOCALITY_DATA_KEY);
                                                         post.setPlaceFrom(new Place((short)sf.getId(), Short.valueOf(cf.getId()), Short.valueOf(tf.getClave()), Double.valueOf(tf.getLatitud()), Double.valueOf(tf.getLongitud())));
                                                         break;
                                                     case PermutaSepWizardModel.CITY_TO_KEY:
-                                                        State st = (State) p.getData().getParcelable(ProfessorCityToPage.STATE_TO_DATA_KEY);
-                                                        City ct = (City) p.getData().getParcelable(ProfessorCityToPage.MUNICIPALITY_TO_DATA_KEY);
-                                                        Town tt = (Town) p.getData().getParcelable(ProfessorCityToPage.LOCALITY_TO_DATA_KEY);
+                                                        State st =  p.getData().getParcelable(ProfessorCityToPage.STATE_TO_DATA_KEY);
+                                                        City ct = p.getData().getParcelable(ProfessorCityToPage.MUNICIPALITY_TO_DATA_KEY);
+                                                        Town tt = p.getData().getParcelable(ProfessorCityToPage.LOCALITY_TO_DATA_KEY);
                                                         post.setPlaceTo(new Place((short)st.getId(), Short.valueOf(ct.getId()), Short.valueOf(tt.getClave()), Double.valueOf(tt.getLatitud()), Double.valueOf(tt.getLongitud())));
                                                         break;
                                                     case PermutaSepWizardModel.POSITION_TYPE_KEY:
