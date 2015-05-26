79d06818bb08541652794848f1f895081c54e9bb
lalongooo <hdez.jeduardo@gmail.com>
2015-05-26 00:43:58 -0500

    Refactored build.gradle (module). Added buildTypes and environment variables

--- app/src/main/java/com/permutassep/rest/permutassep/PermutasSEPRestClient.java
+++ app/src/main/java/com/permutassep/rest/permutassep/PermutasSEPRestClient.java
@@ -3,15 +3,11 @@ package com.permutassep.rest.permutassep;
 import android.util.Base64;
 
 import com.lalongooo.permutassep.BuildConfig;
-import com.permutassep.config.Config;
 
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.converter.Converter;
 
-/**
- * Created by jorge.hernandez on 2/24/2015.
- */
 public class PermutasSEPRestClient {
 
     private static IPermutasSEPService restClient;
@@ -19,13 +15,13 @@ public class PermutasSEPRestClient {
 
     public PermutasSEPRestClient() {
         RestAdapter.Builder builder = new RestAdapter.Builder();
-        builder.setEndpoint(BuildConfig.PERMUTAS_SEP_REST_BASE_URL);
+        builder.setEndpoint(BuildConfig.com_permutassep_api_rest_endpoint);
 
         if(BuildConfig.DEBUG){
             builder.setRequestInterceptor(new RequestInterceptor() {
                 @Override
                 public void intercept(RequestFacade request) {
-                    String credentials = BuildConfig.PERMUTAS_SEP_REST_AUTH_U + ":" + BuildConfig.PERMUTAS_SEP_REST_AUTH_P;
+                    String credentials = BuildConfig.com_permutassep_api_rest_user + ":" + BuildConfig.com_permutassep_api_rest_password;
                     request.addHeader("Accept", "application/json");
                     request.addHeader("Authorization", "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP));
                 }
@@ -38,14 +34,14 @@ public class PermutasSEPRestClient {
 
     public PermutasSEPRestClient(Converter converter) {
         RestAdapter.Builder builder = new RestAdapter.Builder();
-        builder.setEndpoint(BuildConfig.PERMUTAS_SEP_REST_BASE_URL);
+        builder.setEndpoint(BuildConfig.com_permutassep_api_rest_endpoint);
         builder.setConverter(converter);
 
         if(BuildConfig.DEBUG){
             builder.setRequestInterceptor(new RequestInterceptor() {
                 @Override
                 public void intercept(RequestFacade request) {
-                    String credentials = BuildConfig.PERMUTAS_SEP_REST_AUTH_U + ":" + BuildConfig.PERMUTAS_SEP_REST_AUTH_P;
+                    String credentials = BuildConfig.com_permutassep_api_rest_user + ":" + BuildConfig.com_permutassep_api_rest_password;
                     request.addHeader("Accept", "application/json");
                     request.addHeader("Authorization", "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP));
                 }
