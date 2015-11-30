3fa93ee1187b79f1dfe395c4f4b2f87c20745739
lalongooo <hdez.jeduardo@gmail.com>
2015-11-30 07:17:22 -0600

    Implementing a clean architecture: Move drawerImageLoader code to private method

--- app/src/main/java/com/permutassep/presentation/AndroidApplication.java
+++ app/src/main/java/com/permutassep/presentation/AndroidApplication.java
@@ -34,7 +34,25 @@ public class AndroidApplication extends Application {
         super.onCreate();
         this.initializeInjector();
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setUpDrawerImageLoader();
 
+    }
+
+    private void initializeInjector() {
+        this.applicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+    }
+
+    public ApplicationComponent getApplicationComponent() {
+        return this.applicationComponent;
+    }
+
+    /**
+     * Setup Drawer ImageLoader to load profile pictures
+     * for the navigation drawer profile.
+     */
+    private void setUpDrawerImageLoader() {
         DrawerImageLoader.init(new AbstractDrawerImageLoader() {
             @Override
             public void set(ImageView imageView, Uri uri, Drawable placeholder) {
@@ -51,18 +69,8 @@ public class AndroidApplication extends Application {
                 return null;
             }
         });
-
     }
 
-    private void initializeInjector() {
-        this.applicationComponent = DaggerApplicationComponent.builder()
-                .applicationModule(new ApplicationModule(this))
-                .build();
-    }
-
-    public ApplicationComponent getApplicationComponent() {
-        return this.applicationComponent;
-    }
 
     /**
      * Retrieves the Tracker object to send hits to of Google Analytics
