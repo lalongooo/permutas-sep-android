0cb1c6e4ee99d6ec155c4d0c5643d47bd7b9f2a6
lalongooo <hdez.jeduardo@gmail.com>
2016-02-26 00:18:20 -0600

    Uncomment testCharge test method

--- compropago-android/src/test/java/com/compropago/android/net/ComproPagoRestClientTest.java
+++ compropago-android/src/test/java/com/compropago/android/net/ComproPagoRestClientTest.java
@@ -1,5 +1,7 @@
 package com.compropago.android.net;
 
+import com.compropago.android.model.ComproPagoChargeRequest;
+import com.compropago.android.model.ComproPagoChargeResponse;
 import com.compropago.android.model.ComproPagoSendSmsResponse;
 import com.compropago.android.model.ComproPagoVerifyChargeResponse;
 
@@ -9,39 +11,39 @@ import static org.junit.Assert.assertNotNull;
 
 public class ComproPagoRestClientTest {
 
-//    @Test
-//    public void testCharge() {
-//
-//
-//        /**
-//         * JSON Example
-//         *
-//         {
-//         "order_id":"SMGCURL1",
-//         "order_price":75.0,
-//         "order_name":"SAMSUNG GOLD CURL",
-//         "image_url":"https://test.amazon.com/5f4373",
-//         "customer_name":"Alejandra Leyva",
-//         "customer_email":"noreply@compropago.com",
-//         "payment_type":"OXXO"
-//         }
-//         *
-//         */
-//
-//        ComproPagoChargeRequest mChargeRequest = new ComproPagoChargeRequest();
-//        mChargeRequest.setOrderId("PS_ORDERID");
-//        mChargeRequest.setOrderPrice(30.0);
-//        mChargeRequest.setOrderName("PSDonation");
-//        mChargeRequest.setImageUrl("http://lh3.googleusercontent.com/fsjhgyYnBZdNnE2CHuWPACuAWfT1XkKCWIN3khCGe27TBUXrVngNXJJUzktuO9kHng=w170-rw");
-//        mChargeRequest.setCustomerName("Jorge E. Hernàndez");
-//        mChargeRequest.setCustomerEmail("hdez.jeduardo@gmail.com");
-//        mChargeRequest.setPaymentType("OXXO");
-//
-//        ComproPagoService comproPagoService = new ComproPagoRestClient().get();
-//        ComproPagoChargeResponse mChargeResponse = comproPagoService.charge(mChargeRequest);
-//        assertNotNull(mChargeResponse);
-//
-//    }
+    @Test
+    public void testCharge() {
+
+
+        /**
+         * JSON Example
+         *
+         {
+         "order_id":"SMGCURL1",
+         "order_price":75.0,
+         "order_name":"SAMSUNG GOLD CURL",
+         "image_url":"https://test.amazon.com/5f4373",
+         "customer_name":"Alejandra Leyva",
+         "customer_email":"noreply@compropago.com",
+         "payment_type":"OXXO"
+         }
+         *
+         */
+
+        ComproPagoChargeRequest mChargeRequest = new ComproPagoChargeRequest();
+        mChargeRequest.setOrderId("PS_ORDERID");
+        mChargeRequest.setOrderPrice(30.0);
+        mChargeRequest.setOrderName("PSDonation");
+        mChargeRequest.setImageUrl("http://lh3.googleusercontent.com/fsjhgyYnBZdNnE2CHuWPACuAWfT1XkKCWIN3khCGe27TBUXrVngNXJJUzktuO9kHng=w170-rw");
+        mChargeRequest.setCustomerName("Jorge E. Hernàndez");
+        mChargeRequest.setCustomerEmail("hdez.jeduardo@gmail.com");
+        mChargeRequest.setPaymentType("OXXO");
+
+        ComproPagoService comproPagoService = new ComproPagoRestClient().get();
+        ComproPagoChargeResponse mChargeResponse = comproPagoService.charge(mChargeRequest);
+        assertNotNull(mChargeResponse);
+
+    }
 
     @Test
     public void verifyCharge(){
