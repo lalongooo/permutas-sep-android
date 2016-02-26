52cd82238ca315a7a40c284dd4662bd5d7a28042
lalongooo <hdez.jeduardo@gmail.com>
2016-02-26 01:46:33 -0600

    Add charge method

--- compropago-android/src/main/java/com/compropago/android/ComproPago.java
+++ compropago-android/src/main/java/com/compropago/android/ComproPago.java
@@ -1,7 +1,60 @@
 package com.compropago.android;
 
+import com.compropago.android.model.ComproPagoChargeRequest;
+import com.compropago.android.model.ComproPagoChargeResponse;
+import com.compropago.android.net.ComproPagoRestClient;
+import com.compropago.android.net.ComproPagoService;
+
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 /**
  * By Jorge E. Hernandez (@lalongooo) 2016
  */
 public class ComproPago {
+
+    private static ComproPago mComproPago;
+    private static ComproPagoService comproPagoService;
+
+    public static ComproPago getInstance() {
+        if (mComproPago == null) {
+            mComproPago = new ComproPago();
+        }
+
+        if (comproPagoService == null) {
+            comproPagoService = new ComproPagoRestClient().get();
+        }
+        return mComproPago;
+    }
+
+    public static void charge(String orderId, double orderPrice, String orderName, String imageUrl, String customerName, String customerEmail, String paymentType, final Callback callback) {
+
+        ComproPagoChargeRequest chargeRequest = new ComproPagoChargeRequest();
+        chargeRequest.setOrderId(orderId);
+        chargeRequest.setOrderPrice(orderPrice);
+        chargeRequest.setOrderName(orderName);
+        chargeRequest.setImageUrl(imageUrl);
+        chargeRequest.setCustomerName(customerName);
+        chargeRequest.setCustomerEmail(customerEmail);
+        chargeRequest.setPaymentType(paymentType);
+
+
+        comproPagoService.charge(chargeRequest, new retrofit.Callback<ComproPagoChargeResponse>() {
+            @Override
+            public void success(ComproPagoChargeResponse mComproPagoChargeResponse, Response response) {
+                callback.onSuccess(mComproPagoChargeResponse);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                callback.onFailure(error.getResponse().getStatus(), error.getMessage());
+            }
+        });
+    }
+
+    interface Callback {
+        void onSuccess(ComproPagoChargeResponse comproPagoChargeResponse);
+
+        void onFailure(int errorCode, String message);
+    }
 }
