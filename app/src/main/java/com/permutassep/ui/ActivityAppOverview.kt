eb297e0644314ff25c720214c7c80599ad89ecfb
Jorge E. Hernández <hdez.jeduardo@gmail.com>
2023-04-29 01:27:56 -0600

    ISSUE 95: Start implementing ktlint by running it on a GitHub action

--- app/src/main/java/com/permutassep/ui/ActivityAppOverview.kt
+++ app/src/main/java/com/permutassep/ui/ActivityAppOverview.kt
@@ -4,8 +4,7 @@ import android.animation.ObjectAnimator
 import android.animation.StateListAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.viewpager.widget.PagerAdapter
